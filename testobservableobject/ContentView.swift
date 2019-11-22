4f76191e2fa66933e53e45ca1249e08140eac443
Steedman Bass <steedman.bass@gmail.com>
2019-11-22 12:36:08 -0500

    Initial Commit

---
+++ testobservableobject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testobservableobject
+//
+//  Created by Steedman Bass on 11/22/19.
+//  Copyright © 2019 Steedman Bass. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
